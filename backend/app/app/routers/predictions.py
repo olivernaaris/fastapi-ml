206c148b98aea754f035d61a2fb23360bad33e36
oliver.naaris <oliver.naaris@planetway.com>
2020-07-13 23:18:29 +0800

    Make get_predictions async

--- backend/app/app/routers/predictions.py
+++ backend/app/app/routers/predictions.py
@@ -10,11 +10,15 @@ async def pong():
     return {"ping": "pong!"}
 
 
+async def _get_prediction(ticker):  # A function defined by async def is a native coprocess object
+    return PredictStocks(ticker)
+
+
 @router.post("/predict", response_model=StockOut, status_code=200)
 async def get_prediction(payload: StockIn):
     ticker = payload.ticker
 
-    prediction_list = PredictStocks(ticker)
+    prediction_list = await _get_prediction(ticker) # Here _get_prediction(ticker) is a native collaboration object
 
     if not prediction_list:
         raise HTTPException(status_code=400, detail="Model not found.")
